7ccde5acb98259f3ea2044069a44c5743a5fd913
Christophe Duparquet <duparq@free.fr>
2015-02-15 17:47:56 +0100

    Updated board definitions

--- examples/boards/attiny85.h
+++ examples/boards/attiny85.h
@@ -1,4 +1,6 @@
 
+/*	Device configuration
+ */
 #define HW_DEVICE			attiny85, 8pdip
 #define HW_DEVICE_CLK_SRC		rc_8MHz
 #define HW_DEVICE_CLK_PSC		1
@@ -7,12 +9,11 @@
 #define HW_DEVICE_DEBUG_WIRE		disabled
 #define HW_DEVICE_WATCHDOG_ALWAYS_ON	no
 #define HW_DEVICE_CLOCK_OUTPUT		disabled
+#define HW_DEVICE_BROWNOUT_DETECTION	2500_2900mV
 
 /*	Settings for Diabolo
  */
-//#define DIABOLO_PIN_RX			hw_pin_rxd
-//#define DIABOLO_PIN_TX			hw_pin_txd
+#define DIABOLO_PIN_RX			hw_pin_2
+#define DIABOLO_PIN_TX			hw_pin_2
 #define DIABOLO_BPS			115200
 #define DIABOLO_RESET_SIGNAL		DTR
-
-#define	PIN_LED				hw_pin_5
