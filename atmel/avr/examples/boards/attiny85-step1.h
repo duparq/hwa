3b4557499a7d193e521e4f6087493a8a87844764
Christophe Duparquet <duparq@free.fr>
2015-08-22 13:26:43 +0200

    Added ATtiny85 board picture. Removed other unused pictures.

--- atmel/avr/examples/boards/attiny85-step1.h
+++
@@ -1,9 +0,0 @@
-
-#define DIABOLO_PIN_RX                  hw_pin_2
-#define DIABOLO_PIN_TX                  hw_pin_2
-
-#define HW_DEVICE_FUSE_EB		0xFE
-#define HW_DEVICE_FUSE_HB		0xDC
-#define HW_DEVICE_FUSE_LB		0xF1
-
-#include <hwa/attiny85_20pu.h>
