18eea2c82601f3652fc1792e83b1158c4e685477
Christophe Duparquet <duparq@free.fr>
2015-09-09 11:45:39 +0200

    New class _io0a for unconnected pins. hw_write() now uses _hw_write__r8_m().

---
+++ atmel/avr/classes/io0a_1.h
@@ -0,0 +1,25 @@
+
+/*  This file is part of the HWA project.
+ *  Copyright (c) 2012,2015 Christophe Duparquet.
+ *  All rights reserved. Read LICENSE.TXT for details.
+ */
+
+/**
+ * @file
+ * @brief I/O (not connected).
+ */
+
+/**
+ * @page atmelavr_io0a Class _io0a: General Purpose Input/Output
+ *
+ * A class `_io0a` object is a not connected pin.
+ */
+#define _hw_class__io0a
+
+
+#define _hw_mthd_hw_bn__io0a                   , _hw_bn_io0a
+#define _hw_bn_io0a(o,i, cn,bn,bp,...)         0
+
+
+#define _hw_mthd_hw_rel__io0a		, _hw_rel_io0a
+#define _hw_rel_io0a(o,...)		HW_ERR("`"#o"` has no relative."))
---
+++ atmel/avr/classes/io0a_2.h
@@ -0,0 +1,27 @@
+
+/* This file is part of the HWA project.
+ * Copyright (c) 2012,2015 Christophe Duparquet.
+ * All rights reserved. Read LICENSE.TXT for details.
+ */
+
+/**
+ * @file
+ * @brief I/O (not connected).
+ */
+
+#define _hw_mthd_hw_config__io0a	, _hw_io0ax
+#define _hw_mthd_hw_read__io0a		, _hw_io0ax
+#define _hw_mthd_hw_write__io0a		, _hw_io0ax
+#define _hw_mthd_hw_toggle__io0a	, _hw_io0ax
+
+#define _hw_io0ax( o, ... )		HW_ERR("`" #o "` is not connected.")
+
+#define _hw_mthd_hwa_config__io0a	, _hwa_io0ax
+#define _hw_mthd_hwa_write__io0a	, _hwa_io0ax
+
+#define _hwa_io0ax( o, ... )		HWA_ERR("`" #o "` is not connected.")
+
+/**
+ * @page atmelavr_io0a
+ * <br>
+ */
