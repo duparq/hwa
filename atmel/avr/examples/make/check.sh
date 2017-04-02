9fc039e94d83ee5fb0eeb05fc9163b2f0f5e462f
Christophe Duparquet <duparq@free.fr>
2017-04-02 16:08:59 +0200

    Standardized validation files.

--- atmel/avr/examples/make/check.sh
+++ atmel/avr/examples/make/check.sh
@@ -52,20 +52,8 @@ fi
 #  No need to go further is this configuration has never been validated
 #
 if ! ls .valid-*-${HW_DEVICE}-${CFG_CRC}-* >/dev/null 2>&1 ; then
-
-    #  Try removing '"' that where not used in previous versions and alter the
-    #  CRC
-    #
-    sed -e 's/"//g' ${OUT}.cfg >${OUT}.cfg.s
-    CFG_CRC=$(${DIABOLO} --crc32 ${OUT}.cfg.s | awk '{print $1}' )
-    if [ "$S_CRC" = "00000000" ] ; then
-	echo "CRC32 generation failed"
-	exit 5
-    fi
-    if ! ls .valid-*-${HW_DEVICE}-${CFG_CRC}-* >/dev/null 2>&1 ; then
-	echo no validation file for this configuration
-	exit 6
-    fi
+    echo no validation file for this configuration
+    exit 6
 fi
 
 #  Build .bin file
@@ -89,12 +77,28 @@ fi
 
 #  No validation file match. GCC version?
 #
-CCVERSION=$(avr-gcc -dumpversion)
+CCVERSION=avr-gcc-$(avr-gcc -dumpversion | sed -e 's/\.//g')
+#CCVERSION=$(make ccversion)
+# CCVERSION=$(avr-gcc --version	\
+# 		   | awk 'NR==1 {x=match($3,/([0-9]+)\.([0-9]+)\.([0-9]+)/,m);	\
+# 	        if(x!=0){print "avr-gcc-"m[1]m[2]m[3]}}')
 
 if ! ls .valid-${CCVERSION}-${HW_DEVICE}-${CFG_CRC}-* >/dev/null 2>&1 ; then
-    echo no validation file for avr-gcc version $CCVERSION
+    echo "no validation file for $CCVERSION"
     exit 6
 fi
 
-echo CRC mismatch $O_CRC
+#  CRC mismatch
+#    Build a diff of assembly
+#
+V=.valid-${CCVERSION}-${HW_DEVICE}-${CFG_CRC}-*
+if [ -z "$V" ] ; then
+    V=.valid-*-${HW_DEVICE}-${CFG_CRC}-*
+fi
+
+grep '^....:' $V | expand | cut -b 20-48 >${OUT}.valid
+grep '^....:' ${OUT}.lst | expand | cut -b 20-48 >${OUT}.invalid
+diff ${OUT}.valid ${OUT}.invalid >${OUT}.valid.diff
+
+echo "CRC mismatch. See ${OUT}.valid.diff"
 exit 8
---
+++ atmel/avr/examples/standardize-validations.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+
+strip() {
+    # Remove all text between build and .text:
+    sed -i '/build/,/.text:/d' $1
+    # Replace multiple blank lines with single
+    sed -i '/^$/N;/^\n$/D' $1
+}
+
+reconf() {
+    # Extract CRC32 of the original configuration from the file name
+    OLD=$(echo $i | egrep -o '[a-f0-9]{8}')
+    # Compute CRC32 for new configuration
+    egrep -h -o '^[_a-zA-Z0-9]+=".+"$' $i >/tmp/conf
+    NEW=$(diabolo --crc32 /tmp/conf)
+    # Rename file
+    NAME=$(echo $i | sed -e "s/$OLD/$NEW/")
+    if [ "$i" != "$NAME" ] ; then
+	echo Rewrite configuration:
+	echo "  $i"
+	echo "  $NAME"
+        mv $i $NAME
+    fi
+}
+
+for i in $* ; do
+    #echo "  $i: "
+    strip $i
+    RECONF=""
+    if egrep -q '[_a-zA-Z0-9]+=[^"]' $i ; then
+	# Quote configuration values
+	perl -i -pe 's/([_a-zA-Z0-9]+)=([\._a-zA-Z0-9]+)/\1="\2"/g' $i
+	RECONF=1
+    fi
+    if grep -q 'hw_pin_' $i ; then
+	# Rename pins
+	perl -i -pe 's/hw_pin_/pin_/g' $i
+	RECONF=1
+    fi
+    if grep -q '="hw_' $i ; then
+	# Remove hw_ prefix from object names
+	perl -i -pe 's/="hw_([a-z0-9]+)"/="\1"/g' $i
+	RECONF=1
+    fi
+    if egrep -q '(="loop_up"|="loop_updown")' $i ; then
+	# Rename counting mode
+	perl -i -pe 's/="loop_up"/="up_loop"/g;' \
+	     -pe 's/="loop_updown"/="updown_loop"/g;' $i
+	RECONF=1
+    fi
+    if egrep -q '(="oc[0-9][0-9]")' $i ; then
+	# Rename compare outputs
+	perl -i -pe 's/="oc([0-9])([0-9])"/="counter\1compare\2"/g;' $i
+	RECONF=1
+    fi
+    if [ -n "$RECONF" ] ; then
+	reconf $i
+    else
+	echo "$i: nothing to do."
+    fi
+done
